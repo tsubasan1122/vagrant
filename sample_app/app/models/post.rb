d0e4229da3917d8f4b984e2300c4adb82fdc7714
tsubasa <tsubasan1122@gmail.com>
2018-08-08 13:55:12 +0900

    Finish 3-10

---
+++ sample_app/app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+
+	attachment :image
+end
---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '8033a14502e34045448d4cd20a91d081ecb82f05a6f18700fac07221a43935313371abc309d55f0b9568877160b4d9524c797be20a3c2f228cfe17a253cd7a29'
