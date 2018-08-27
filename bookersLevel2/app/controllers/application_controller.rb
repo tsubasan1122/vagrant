3974466ff2e8faf16b307d6060df5c994974f6ee
tsubasa <tsubasan1122@gmail.com>
2018-08-27 17:09:46 +0900

    sign_up修正

--- bookersLevel2/app/controllers/application_controller.rb
+++ bookersLevel2/app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ def after_sign_in_path_for(resource_or_scope)
  	
  protected
  def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
  end
